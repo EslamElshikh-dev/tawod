e4a6a8a2c461b8e128b8022e9dda943d95c62622
Eslam Elshikh <moqawel1215@gmail.com>
2026-08-31 19:09:22 +0300

    Restore strict export parity for homepage

--- app/route.ts
+++ app/route.ts
@@ -1,8 +1,7 @@
-import { withAdsRescue } from "@/lib/ad-rescue-html";
 import { htmlResponse } from "@/lib/legacy-pages";
 
 export const dynamic = "force-static";
 
-export async function GET() {
-  return withAdsRescue(htmlResponse("index.html"));
+export function GET() {
+  return htmlResponse("index.html");
 }
